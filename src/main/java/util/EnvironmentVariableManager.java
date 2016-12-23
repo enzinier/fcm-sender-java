714ffb865038cb491ea53da4549ee777c652d23f
Engineer Jason <enzinier@gmail.com>
2016-12-23 17:37:29 +0900

    Extract auth key to get from environment variable manager.

---
+++ src/main/java/util/EnvironmentVariableManager.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class EnvironmentVariableManager {
+
+  private static EnvironmentVariableManager instance = null;
+
+  public static EnvironmentVariableManager getInstance() {
+    if (instance == null) {
+      instance = new EnvironmentVariableManager();
+    }
+    return instance;
+  }
+
+  public Optional<String> getEnvironmentVariable(String keyName) {
+    Map<String, String> env = System.getenv();
+    for (String envName : env.keySet()) {
+      if (envName.equals(keyName)) {
+        return Optional.of(env.get(envName));
+      }
+    }
+
+    return null;
+  }
+}
